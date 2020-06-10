004d5a208581877bdc0d7fc7a617d7a020b1ddc1
N0rf3n <n0rf3n@N0rf3ns-iMac-Pro.local>
2020-06-10 06:14:44 -0500

    Se agrega State class y Widget Stateful para la generación de palabras

--- 01 StatefulWidget/statefulwidget/lib/main.dart
+++ 01 StatefulWidget/statefulwidget/lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyAppN0rf3n());
 class MyAppN0rf3n extends StatelessWidget {//StatelessWidget -> Hace la app un widget
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); //Generación random de palabras
     return MaterialApp(
       title: 'Welcome to App N0rf3n',
       home: Scaffold( //Provee una AppBar por defecto
@@ -14,9 +13,23 @@ class MyAppN0rf3n extends StatelessWidget {//StatelessWidget -> Hace la app un w
           title: Text('Welcome to Flutter N0rf3n'),
         ),
         body: Center(           
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> { //State class
+  // TODO Add build() method
+    @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {//Widget stateful
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }

775c1b196b384374820bf263d8002c18e5cca3a0
N0rf3n <n0rf3n@N0rf3ns-iMac-Pro.local>
2020-06-09 19:20:00 -0500

    Se agrega una librería.

--- 01 StatefulWidget/statefulwidget/lib/main.dart
+++ 01 StatefulWidget/statefulwidget/lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyAppN0rf3n());
 
-class MyAppN0rf3n extends StatelessWidget {
+class MyAppN0rf3n extends StatelessWidget {//StatelessWidget -> Hace la app un widget
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); //Generación random de palabras
     return MaterialApp(
       title: 'Welcome to App N0rf3n',
-      home: Scaffold(
+      home: Scaffold( //Provee una AppBar por defecto
         appBar: AppBar(
           title: Text('Welcome to Flutter N0rf3n'),
         ),
-        body: Center(
-          child: Text('Hello World.'),
+        body: Center(           
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );

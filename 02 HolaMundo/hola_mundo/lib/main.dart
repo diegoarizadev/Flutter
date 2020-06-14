f5d00363d9e78df81a16c80431988e344b39e200
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-14 18:43:36 -0500

    Se agrega Scaffold y un tema

--- 02 HolaMundo/hola_mundo/lib/main.dart
+++ 02 HolaMundo/hola_mundo/lib/main.dart
@@ -1,22 +1,35 @@
-
 import 'package:flutter/material.dart'; //Este Import es necesario para mostrar la interface grafica estandar de flutter
 
-void main() => runApp(new N0rf3nApp());//Función principal de dart para realizar las ejecuciones. runApp esta esperando un widget para inflar la vista
+void main() => runApp(
+    new N0rf3nApp()); //Función principal de dart para realizar las ejecuciones. runApp esta esperando un widget para inflar la vista
 
 //Widget para inflar la vista
-class N0rf3nApp extends StatelessWidget{ //StatelessWidget!
-  
+class N0rf3nApp extends StatelessWidget {
+  //StatelessWidget!
+
   @override
-  build(context){ //Context contiene la información del arbol de widgets, temas,  padres, hijos.. y debe retornar un widget
+  build(context) {
+    //Context contiene la información del arbol de widgets, temas,  padres, hijos.. y debe retornar un widget
 
     return MaterialApp(
-       home : //Material recibe este parametro obligatoriamente.
-       Center( //Este widget hace el centro de los hijos que contenga.
-         child : // parametro o widget obligatorio.
-          Text( 'Hola Mundo'),// Este es un widget e tipo texto.
-         ),  
+      title: 'Hola N0rf3n',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: //Material recibe este parametro obligatoriamente.
+          Scaffold(
+        appBar: AppBar(
+          title: Text('Hola Mundo - N0rf3n'),
+        ),
+        body: Center(
+          //Este widget hace el centro de los hijos que contenga.
+          child: // parametro o widget obligatorio.
+              Text(
+            'Hola Mundo',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ), // Este es un widget e tipo texto.
+        ),
+      ),
     );
-   
   }
-
 }
--- README.md
+++ README.md
@@ -5,8 +5,7 @@
 
 #00 Dart
 #01 StatefulWidget
-
-
 ![](https://n0rf3n.github.io/Flutter/Resources/StatefulWidget.gif)
 
-#02 xx
+#02 Hola Mundo
+![](https://n0rf3n.github.io/Flutter/Resources/HolaMundo.gif)

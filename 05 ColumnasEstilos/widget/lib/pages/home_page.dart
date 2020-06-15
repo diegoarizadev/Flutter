e482702ac1a80fd1f78fb1f51068c5eb2995df4f
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-14 23:18:17 -0500

    Se agregan estilos custom para los Text y el objecto columnas.

--- 05 ColumnasEstilos/widget/lib/pages/home_page.dart
+++ 05 ColumnasEstilos/widget/lib/pages/home_page.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 class HomePage extends StatelessWidget {
+  final styleTittle = new TextStyle(
+    //Instancia Objectos globales para ser reutilizados.
+    fontSize: 20,
+    fontStyle: FontStyle.normal,
+    color: Colors.black,
+  );
+
+  final styleSubTittle = new TextStyle(
+      fontSize: 15, fontStyle: FontStyle.normal, color: Colors.red[300]);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //Scaffold es la estructura de toda la pantalla.
       appBar: AppBar(
         title: (Text('Titulo AppBar')),
-        centerTitle: true, //Para Android no se debe marcar esta propiedad, de lo contrario no queda centrado.
+        centerTitle:
+            true, //Para Android no se debe marcar esta propiedad, de lo contrario no queda centrado.
         elevation: 100.0, //Sombra debajo del AppBar.
       ),
       body: Center(
-        child: Text('Esto es un widget'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment
+              .center, //con esta propiedad centra el Widget en toda la pantalla.
+          children: <Widget>[
+            //Se pasa una lista de widgets.
+            Text(
+              'Texto 1',
+              style: styleTittle,
+            ),
+            Text(
+              'Texto 2',
+              style: styleSubTittle,
+            ),
+          ],
+        ),
       ),
     );
   }

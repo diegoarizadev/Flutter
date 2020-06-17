f4c7fed44b7148ae8f306cee577aaad8a847e211
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-17 07:07:29 -0500

    Se agrega función para crear las celdas o elementos de listView, se agrega un divisor custom y se utiliza el operador cascada de dart para agregar elementos

--- 10 ListTile/componentes/lib/src/pages/home_temp.dart
+++ 10 ListTile/componentes/lib/src/pages/home_temp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePageTmp extends StatelessWidget {
+  final elementsList = ['A', 'B', 'C', 'D', 'E'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,30 +10,37 @@ class HomePageTmp extends StatelessWidget {
         title: Text('Temp'),
       ),
       body: ListView(
-        //implementación de un listView
-        children: <Widget>[
-          //recibe cualquier elemento o Widget
-          ListTile( //Simula los elementos de una lista de contactos 'celdas'. 
-            title: Text('List titulo'),
-          ),
-          Divider(),//Crea la linea o separacion entre las celdas, este objecto recibe cualquier tipo de Widget, así que la customización será más libre.
-          ListTile(
-            title: Text('List titulo'),
-          ),
-          Divider(),//Crea la linea o separacion entre las celdas.
-          ListTile(
-            title: Text('List titulo'),
-          ),
-          Divider(),//Crea la linea o separacion entre las celdas.
-          ListTile(
-            title: Text('List titulo'),
-          ),
-          Divider(),//Crea la linea o separacion entre las celdas.
-          ListTile(
-            title: Text('List titulo'),
-          ),
-        ],
-      ),
+          //implementación de un listView
+          children: _createlist()), //Items o celdas de la list a mostrar.
     );
+  }
+
+  List<Widget> _createlist() {
+    List<Widget> list = new List<
+        Widget>(); // como no se define un largo se convierte una lista dinamica.
+
+    //Recorrer la lista elementsList
+
+    for (String item in elementsList) {
+      //El tipo de objeto es String.!
+
+      final listFinal = ListTile(
+        //Se define el objecto o Celdas de la lista para almacenar la información.
+        title: Text(item),
+      );
+
+      //list.add(listFinal); //El objeto listFinal debe ser agregado/insertado al elemento list
+      //list.add(Divider());//Se agrega la linea para dividir las celdas
+
+      //**OPERADOR DE CASCADA EN DART  */ Agrega varios elementos con el doble ..
+      list
+        ..add(listFinal)
+        ..add(Divider(
+          height: 50.0, //Define el alto del divisor.
+          color: Colors.blue, //Define el color del divisor.
+        ));
+    }
+
+    return list;
   }
 }

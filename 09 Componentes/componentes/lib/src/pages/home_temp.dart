84855ce962356337dac52e1795c0f5f5eac2d85f
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-17 06:30:44 -0500

    Se agrega ListTile como celda para la lista y Divider para los divisores o separadores de la celdas

--- 09 Componentes/componentes/lib/src/pages/home_temp.dart
+++ 09 Componentes/componentes/lib/src/pages/home_temp.dart
@@ -7,9 +7,29 @@ class HomePageTmp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Temp'),
       ),
-      body: ListView(//implementación de un listView
-        children: <Widget>[//recibe cualquier elemento o Widget
-          Text('Hola'),
+      body: ListView(
+        //implementación de un listView
+        children: <Widget>[
+          //recibe cualquier elemento o Widget
+          ListTile( //Simula los elementos de una lista de contactos 'celdas'. 
+            title: Text('List titulo'),
+          ),
+          Divider(),//Crea la linea o separacion entre las celdas, este objecto recibe cualquier tipo de Widget, así que la customización será más libre.
+          ListTile(
+            title: Text('List titulo'),
+          ),
+          Divider(),//Crea la linea o separacion entre las celdas.
+          ListTile(
+            title: Text('List titulo'),
+          ),
+          Divider(),//Crea la linea o separacion entre las celdas.
+          ListTile(
+            title: Text('List titulo'),
+          ),
+          Divider(),//Crea la linea o separacion entre las celdas.
+          ListTile(
+            title: Text('List titulo'),
+          ),
         ],
       ),
     );

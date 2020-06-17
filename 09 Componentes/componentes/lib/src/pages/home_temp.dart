24a6511df3bcbe0a9b06aad8ed28b33844e2252a
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-17 06:11:41 -0500

    Modificaciones iniciales.

---
+++ 09 Componentes/componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomePageTmp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Buenos y madrugados días.'),);
+  }
+}

bae89ef66de8f2365a507ddf8fd5fa425c5d2aa7
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-19 06:34:38 -0500

    Se realiza implementación del Future Builder

--- 15 Future Builder/componentes/lib/src/pages/home_page.dart
+++ 15 Future Builder/componentes/lib/src/pages/home_page.dart
@@ -17,42 +17,66 @@ class HomePage extends StatelessWidget {
 
   Widget _listCustom() //Body del Scaffold
   {
-    //print(menuProvider.opcionesMenu); //Al ejecutar el constructor de este objeto se imprime en la consola la información del JSON
+    /*IMPLEMENTACION DEL FUTURE BUILDER*
+    
+    Un future tiene 3 estados
 
-    menuProvider.loadData().then((opciones) {
-      print('_listCustom()');
-      print('$opciones');
-    }); //loaddata retorna un future, entonces con el .then se queda a la escucha hasta que responda.
-    return ListView(
-      children: _itemsList(),
+    1. Pidiendo informacion 
+    2. Cuando resuelve  
+    3. Error 
+
+     */
+
+    return FutureBuilder(
+      future:
+          menuProvider //Esta enlazado o lo que estamos esperando, ejemplo loadData de tipo Future
+              .loadData(),
+      initialData: [], //Información que va a conterner mientras espera la información
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        /*    
+      Este builder se ejecutara en las 3 etapas
+
+      1. Pidiendo informacion 
+      2. Cuando resuelve  
+      3. Error 
+      */
+
+        print(snapshot.data); //snapshot.data contiene la data del JSON
+        return ListView(
+          children: _itemsList(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _itemsList() //Retorna los items o celdas de la lista.
+  List<Widget> _itemsList(
+      List<dynamic> snapshotData) //Retorna los items o celdas de la lista.
   {
     double heightCell = 15.0;
-    return [
-      ListTile(
-        title: Text('Item A'),
-      ),
-      Divider(
-        height: heightCell, //Define el alto del divisor.
-        color: Colors.blue, //Define el color del divisor.
-      ),
-      ListTile(
-        title: Text('Item A'),
-      ),
-      Divider(
-        height: heightCell, //Define el alto del divisor.
-        color: Colors.blue, //Define el color del divisor.
-      ),
-      ListTile(
-        title: Text('Item A'),
-      ),
-      Divider(
-        height: heightCell, //Define el alto del divisor.
-        color: Colors.blue, //Define el color del divisor.
-      )
-    ];
+
+    final List<Widget> opciones = [];
+
+    snapshotData.forEach((item) {
+      final cell = ListTile(
+        title: Text(item['texto']),
+        leading: Icon(
+          Icons.accessibility,
+          color: Colors.green,
+        ),
+        trailing: Icon(Icons.arrow_back, color: Colors.green),
+        onTap: () {},
+      );
+
+      opciones
+        ..add(cell)
+        ..add(
+          Divider(
+            height: heightCell, //Define el alto del divisor.
+            color: Colors.blue, //Define el color del divisor.
+          ),
+        );
+    });
+
+    return opciones;
   }
 }

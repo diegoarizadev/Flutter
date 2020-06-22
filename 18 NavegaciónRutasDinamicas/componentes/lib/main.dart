c29b5f0523ec0f364e3b366687568d05f7553d94
Diego Ariza <Diego.ariza2@gmail.com>
2020-06-22 18:04:00 -0500

    Se agrega clase para definir las rutas de la App

--- 18 NavegaciónRutasDinamicas/componentes/lib/main.dart
+++ 18 NavegaciónRutasDinamicas/componentes/lib/main.dart
@@ -1,29 +1,26 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
 
-  @override
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override 
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false, //Quita el banner "Debug"
       //definición de las rutas.
       initialRoute: '/', //Inicializador.
-      routes: <String, WidgetBuilder>{ //Se pasa un mapa con las rutas.
-      '/' : (BuildContext context) => HomePage(), //configuración de las rutas.
-      'alert' : (BuildContext context) => AlertPage(),
-      'avatar' : (BuildContext context) => AvatarPage(),
-      },
-      onGenerateRoute: (RouteSettings settings){
-
-        print('Ruta invocada ${settings.name}');
-
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        //Como se invoca una ruta que no esta definida se disparara el MaterialPageRoute
+        print(
+            'Ruta invocada ${settings.name}'); //imprime el nombre de la ruta que esta invocando o solicitando.
+        return MaterialPageRoute(
+            //retorna a una pagina.
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
-} 
+}
---
+++ 18 NavegaciónRutasDinamicas/componentes/lib/src/routes/routes.dart
@@ -0,0 +1,13 @@
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() {
+  return <String, WidgetBuilder>{
+    //Se pasa un mapa con las rutas.
+    '/': (BuildContext context) => HomePage(), //configuración de las rutas.
+    'alert': (BuildContext context) => AlertPage(),
+    'avatar': (BuildContext context) => AvatarPage(),
+  };
+}

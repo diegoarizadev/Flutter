33a3c5db5dad13eb031c88a1c9174b71e78a80ed
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-05-27 09:47:41 -0500

    Separación de lógica en directorios

--- 04 SeparaciónLógica/contador/lib/main.dart
+++ 04 SeparaciónLógica/contador/lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:flutter/material.dart'; //Este es el material grafico estandar
+import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
   runApp(
       new MyApp()); //Inicializa la App. infla un widget para pintarlo en la pantalla.
-}
-
-//creacion de un widget
-class MyApp extends StatelessWidget {
-  //
-  @override
-  Widget build(BuildContext context) {
-    //context contiene todo el contexto del arbol de objectos, herencias
-    return MaterialApp(
-      //debe retornar un widget, MaterialApp widget para la configuracion global de la App
-      home: Center(
-        child: Text('Hola Mundo'),
-      ),
-    );
-  }
 }

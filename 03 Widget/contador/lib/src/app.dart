1edd92f3c729540ce535484f35353d9e70df3e58
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-05-27 09:50:18 -0500

    Se agrega proyecto nuevo para realizar la implementacion de un Scaffold

---
+++ 03 Widget/contador/lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//creacion de un widget
+class MyApp extends StatelessWidget {
+  //
+  @override
+  Widget build(BuildContext context) {
+    //context contiene todo el contexto del arbol de objectos, herencias
+    return MaterialApp(
+      //debe retornar un widget, MaterialApp widget para la configuracion global de la App
+      home: Center(
+        child: Text('Hola Mundo!'),
+      ),
+    );
+  }
+}

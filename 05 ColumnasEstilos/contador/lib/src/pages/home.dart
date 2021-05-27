bca0fe1daabfa23514c13e4eb470b11f11022881
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-05-27 10:53:13 -0500

    Se agrega un widget de tipo columna, con estilos custom para los textos.

--- 05 ColumnasEstilos/contador/lib/src/pages/home.dart
+++ 05 ColumnasEstilos/contador/lib/src/pages/home.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+// Creacion de un estilo.
+  final TextStyle styleCustomTittle = new TextStyle(
+      fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 20);
+
+  final TextStyle styleCustomtext = new TextStyle(
+      fontWeight: FontWeight.normal, fontStyle: FontStyle.normal, fontSize: 25);
+
   @override
   Widget build(BuildContext context) {
     // este es un widget que tiene inmerso un Appbar (inferior), un Bar, un Action Button y ventanas laterales.
@@ -11,7 +18,16 @@ class Home extends StatelessWidget {
         elevation: 50.0,
       ),
       body: Center(
-        child: Text('Texto en el centro de la vista'),
+        child: Column(
+          children: [
+            Text('Números de clicks:', style: styleCustomTittle),
+            Text(
+              '0',
+              style: styleCustomtext,
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
       ),
     );
   }

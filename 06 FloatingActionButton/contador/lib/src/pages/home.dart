9313b73c75ae3c330930ce21e257a8d098eabd10
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-05-27 11:06:14 -0500

    Se agrega proyecto y logica para la implementación de un Widget floatingActionButton, una función anonima y sus recursos graficos

---
+++ 06 FloatingActionButton/contador/lib/src/pages/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+// Creacion de un estilo.
+  final TextStyle styleCustomTittle = new TextStyle(
+      fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 20);
+
+  final TextStyle styleCustomtext = new TextStyle(
+      fontWeight: FontWeight.normal, fontStyle: FontStyle.normal, fontSize: 25);
+
+  @override
+  Widget build(BuildContext context) {
+    // este es un widget que tiene inmerso un Appbar (inferior), un Bar, un Action Button y ventanas laterales.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo del App bar'),
+        centerTitle: true,
+        elevation: 50.0,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Números de clicks:', style: styleCustomTittle),
+            Text(
+              '0',
+              style: styleCustomtext,
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Se presionado el botón');
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}

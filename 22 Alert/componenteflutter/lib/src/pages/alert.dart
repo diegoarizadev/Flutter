4cff597bb1cbbd97e3c34abd94d8862507956d53
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-05-30 12:33:57 -0500

    Se agrega toda la lógica necesaria para la implementación de un Alert

--- 22 Alert/componenteflutter/lib/src/pages/alert.dart
+++ 22 Alert/componenteflutter/lib/src/pages/alert.dart
@@ -17,7 +17,14 @@ class AlertPage extends StatelessWidget {
         ),
         backgroundColor: Colors.green,
       ),
-      body: ListTile(),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Ejecutar Alert!'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green, shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
@@ -26,5 +33,39 @@ class AlertPage extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
     );
+  }
+
+  _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible:
+            true, //Este es un parametro para permitir salir del alert en caso de tocar por fuera de el.
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            title: Text('Titulo Alert'),
+            content: Column(
+              mainAxisSize: MainAxisSize
+                  .min, //Sin este paremetro la columna toma todo el espacio de la pantalla. al tomar la opcion .min ocupa el minimo posible
+              children: [
+                Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+                FlutterLogo(size: 90.0)
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                  onPressed: () => Navigator.of(context)
+                      .pop(), //Función para cerrar el Alert
+                  child: Text('Botón 1 Alert!')),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }, //Función para cerrar el Alert
+                  child: Text('Botón 2 Alert!'))
+            ],
+          );
+        });
   }
 }

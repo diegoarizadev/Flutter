9c4439f762bb971cdee322ae49dac867f4439a2d
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-05-31 09:21:57 -0500

    Se agrega toda la lógica para la implementación de un circleAvatar

--- 23 Circle avatar/componenteflutter/lib/src/pages/avatar.dart
+++ 23 Circle avatar/componenteflutter/lib/src/pages/avatar.dart
@@ -11,13 +11,37 @@ class AvartarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          Container(
+            padding: EdgeInsets.all(6.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://st4.depositphotos.com/27867620/30730/v/600/depositphotos_307309712-stock-illustration-godfather-web-icon-vector-illustration.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('DA'),
+              backgroundColor: Colors.orange,
+            ),
+          )
+        ],
         title: Text(
           'Avatars',
           strutStyle: strutStyleBar,
         ),
         backgroundColor: Colors.green,
       ),
-      body: ListTile(),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://avatarfiles.alphacoders.com/159/159736.jpg'),
+          placeholder: AssetImage('assets/placeholder.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
